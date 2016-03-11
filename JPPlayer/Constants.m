be964e26bb2d0f0ae17c436df53c0ce6842ce5cd
Prime <jimmylevelup@gmail.com>
2016-03-11 13:53:34 +0800

    Add a pseudo constants.m that you should fill your URLs

---
+++ JPPlayer/Constants.m
@@ -0,0 +1,39 @@
+//
+//  Constants.m
+//  JPPlayer
+//
+//  Created by Prime on 12/11/15.
+//  Copyright © 2015 Prime. All rights reserved.
+//
+
+#include "Constants.h"
+
+// Spotify
+NSString *SpotifySessionKey = @"SpotifySession";
+NSString *SpotifyClientId = @"YOUR ID";
+NSString *SpotifyRedirectURL = @"YOUR REDIRECT URL"; // show match URL scheme at info page
+NSString *SpotifySwapURL = @"YOUR SWAP URL";
+NSString *SpotifyRefreshURL = @"YOUR REFRESH URL";
+
+CGFloat AnimationInterval = .3f;
+
+// NSUserDefaults Keys
+NSString *UserDefaultsThemeColorKey = @"UserDefaultsThemeColorKey";
+
+// View Controller Component
+CGFloat LeftBarWidth = 88.f;
+CGFloat PlayerViewHeight = 60.f;
+
+// Player View Controller Component
+CGFloat SmallButtonWidth = 30.f;
+CGFloat LargeButtonWidth = 50.f;
+CGFloat PlayButtonWidth = 80.f;
+CGFloat TimeLabelWidth = 40.f;
+
+// List View Controller
+CGFloat ContainerWidth = 500.f;
+CGFloat FakeHeaderHeight = 100.f;
+
+// Popup Player View Controller
+CGFloat CoverLength = 600.f;
+CGFloat CoverInset = 25.f;
